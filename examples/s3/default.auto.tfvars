6e05c91d84316f3897d0755fe572775aa6d4aaa8
Young-ook <youngook.kim@gmail.com>
2021-12-02 20:07:55 +0900

    feat(s3): s3 bucket module (#15)
    * feat(s3): s3 bucket module
    
    * feat(notebook): s3 bucket and bucket policy example
    
    * feat(s3): s3 example and test cases
    
    * docs(s3): correct project name
    
    * fix(s3): removed useless variables

---
+++ examples/s3/default.auto.tfvars
@@ -0,0 +1,7 @@
+aws_region      = "ap-northeast-2"
+force_destroy   = false
+versioning      = false
+lifecycle_rules = []
+tags = {
+  env = "dev"
+}
--- examples/s3/variables.tf
+++ examples/s3/variables.tf
@@ -4,25 +4,6 @@
 variable "aws_region" {
   description = "The aws region to deploy"
   type        = string
-  default     = "us-east-1"
-}
-
-variable "use_default_vpc" {
-  description = "A feature flag for whether to use default vpc"
-  type        = bool
-  default     = false
-}
-
-variable "azs" {
-  description = "A list of availability zones for the vpc to deploy resources"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-variable "subnets" {
-  description = "The subnet IDs to deploy"
-  type        = list(string)
-  default     = null
 }
 
 ### s3
@@ -43,22 +24,11 @@ variable "lifecycle_rules" {
   default     = []
 }
 
-### sagemaker
-variable "sagemaker_studio" {
-  description = "Amazon SageMaker studio definition"
-  default     = {}
-}
-
-variable "notebook_instances" {
-  description = "Notebook instances definition"
-  default     = []
-}
-
 ### description
 variable "name" {
   description = "The logical name of the module instance"
   type        = string
-  default     = "eks"
+  default     = null
 }
 
 ### tags
