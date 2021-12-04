b0596481ca2e88115e0a6f1090760286835ec894
Young-ook Kim <youngook.kim@gmail.com>
2021-12-04 10:10:28 +0900

    feat(vpc): vpc subnet types - isolated, public, standard

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,16 +2,17 @@
 
 output "vpc" {
   description = "The attributes of the default VPC Id"
-  value       = (local.use_default_vpc ? data.aws_vpc.default.0 : aws_vpc.vpc.0)
+  value       = local.vpc
 }
 
 output "subnets" {
   description = "A list of subnet IDs in the default VPC"
-  value = (local.use_default_vpc ?
+  value = (local.default_vpc ?
     {
       public = { for net in data.aws_subnet.default : net.availability_zone => net.id }
     } :
     {
+      public  = { for k, v in aws_subnet.public : k => v.id }
       private = { for k, v in aws_subnet.private : k => v.id }
     }
   )
@@ -19,11 +20,12 @@ output "subnets" {
 
 output "route_tables" {
   description = "A list of subnet IDs in the default VPC"
-  value = (local.use_default_vpc ?
+  value = (local.default_vpc ?
     {
       public = { main = data.aws_vpc.default.0.main_route_table_id }
     } :
     {
+      public  = { for k, v in aws_route_table.public : k => v.id }
       private = { for k, v in aws_route_table.private : k => v.id }
     }
   )
