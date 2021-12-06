5dd86446effafb7cda638550daba2d385fc08892
Young-ook Kim <youngook.kim@gmail.com>
2021-12-06 11:43:20 +0900

    feat(vpc): vpc endpoint

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,26 +1,8 @@
 ### output variables
 
-output "vpc" {
-  description = "The attributes of the default VPC Id"
-  value       = local.vpc
-}
-
-output "subnets" {
-  description = "A list of subnet IDs in the default VPC"
-  value = (local.default_vpc ?
-    {
-      public = { for net in data.aws_subnet.default : net.availability_zone => net.id }
-    } :
-    {
-      public  = { for k, v in aws_subnet.public : k => v.id }
-      private = { for k, v in aws_subnet.private : k => v.id }
-    }
-  )
-}
-
-output "route_tables" {
-  description = "A list of subnet IDs in the default VPC"
-  value = (local.default_vpc ?
+locals {
+  vpc = local.default_vpc ? data.aws_vpc.default.0 : aws_vpc.vpc.0
+  route_tables = (local.default_vpc ?
     {
       public = { main = data.aws_vpc.default.0.main_route_table_id }
     } :
@@ -29,4 +11,34 @@ output "route_tables" {
       private = { for k, v in aws_route_table.private : k => v.id }
     }
   )
+  subnets = (local.default_vpc ?
+    {
+      public = { for net in data.aws_subnet.default : net.availability_zone => net.id }
+    } :
+    {
+      public  = { for k, v in aws_subnet.public : k => v.id }
+      private = { for k, v in aws_subnet.private : k => v.id }
+    }
+  )
+  vpce_subnets = local.default_vpc || local.public ? local.subnets.public : local.subnets.private
+}
+
+output "vpc" {
+  description = "Attributes of the VPC"
+  value       = local.vpc
+}
+
+output "subnets" {
+  description = "A list of subnet IDs in the VPC"
+  value       = local.subnets
+}
+
+output "route_tables" {
+  description = "A list of subnet IDs in the VPC"
+  value       = local.route_tables
+}
+
+output "vpce" {
+  description = "Attributes of the VPC endpoints"
+  value       = aws_vpc_endpoint.vpce
 }
