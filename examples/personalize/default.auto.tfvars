6d99e975f34528681afbcc932cd75323fccaecc3
Young-ook Kim <youngook.kim@gmail.com>
2021-12-29 18:07:22 +0900

    feat(personalize): personalize with sagemaker example

--- examples/personalize/default.auto.tfvars
+++ examples/personalize/default.auto.tfvars
@@ -1,7 +1,5 @@
-aws_region      = "ap-northeast-2"
-azs             = ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"]
-name            = "sagemaker-personalize"
-use_default_vpc = true
+aws_region = "ap-northeast-2"
+name       = "sagemaker-personalize"
 tags = {
   env = "dev"
 }
--- examples/personalize/main.tf
+++ examples/personalize/main.tf
@@ -10,8 +10,22 @@ provider "aws" {
 
 # sagemaker
 module "sagemaker" {
-  source             = "../../"
-  name               = var.name
-  tags               = var.tags
-  notebook_instances = var.notebook_instances
+  source = "../../"
+  name   = var.name
+  tags   = var.tags
+  notebook_instances = [
+    {
+      name                    = "default"
+      instance_type           = "ml.m5.xlarge"
+      default_code_repository = aws_sagemaker_code_repository.repo.code_repository_name
+    }
+  ]
+}
+
+# personalize sample
+resource "aws_sagemaker_code_repository" "repo" {
+  code_repository_name = "amazon-personalize-samples"
+  git_config {
+    repository_url = "https://github.com/aws-samples/amazon-personalize-samples.git"
+  }
 }
