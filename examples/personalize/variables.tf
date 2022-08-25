87218395aa31a6140c2d60849b3a472a23c14e6c
Young-ook Kim <youngook.kim@gmail.com>
2022-08-25 13:51:14 +0900

    fix(personalize): remove useless variables

--- examples/personalize/variables.tf
+++ examples/personalize/variables.tf
@@ -7,24 +7,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "use_default_vpc" {
-  description = "A feature flag for whether to use default vpc"
-  type        = bool
-  default     = false
-}
-
-variable "azs" {
-  description = "A list of availability zones for the vpc to deploy resources"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-variable "subnets" {
-  description = "The subnet IDs to deploy"
-  type        = list(string)
-  default     = null
-}
-
 ### sagemaker
 variable "studio" {
   description = "Amazon SageMaker studio definition"
