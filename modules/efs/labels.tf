7ed13ad0f83b184f56937248848b748ccc0201e2
youngookkim <youngook.kim@gmail.com>
2022-08-27 13:58:03 +0900

    feat(efs): update naming rule (#50)

--- modules/efs/labels.tf
+++ modules/efs/labels.tf
@@ -1,15 +1,13 @@
-resource "random_string" "uid" {
-  length  = 12
-  upper   = false
-  lower   = true
-  number  = false
-  special = false
+### frigga name
+module "frigga" {
+  source  = "Young-ook/spinnaker/aws//modules/frigga"
+  version = "2.3.5"
+  name    = var.name == null || var.name == "" ? "efs" : var.name
+  petname = var.name == null || var.name == "" ? true : false
 }
 
 locals {
-  service = "efs"
-  uid     = join("-", [local.service, random_string.uid.result])
-  name    = var.name == null || var.name == "" ? local.uid : var.name
+  name = module.frigga.name
   default-tags = merge(
     { "terraform.io" = "managed" },
     { "Name" = local.name },
